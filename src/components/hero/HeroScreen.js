ad5367e1b63f04ad31c60be7d8826dad9f4515eb
Kirsty Brown <kirsty.a.d.brown@gmail.com>
2022-02-14 16:59:34 +0100

    added useParams and useNavigate to HeroScreen

--- src/components/hero/HeroScreen.js
+++ src/components/hero/HeroScreen.js
@@ -1,9 +1,60 @@
-import React from 'react'
+import { Navigate, useNavigate, useParams } from 'react-router-dom';
+import { getHeroById } from '../../selectors/getHeroById';
 
 export const HeroScreen = () => {
+
+  const { heroId } = useParams();
+
+  const navigate = useNavigate();
+
+  const hero = getHeroById(heroId);
+
+  const handleReturn = () => {
+    return navigate('/');
+  }
+
+  if (!hero) return <Navigate to='/' />
+
+  const {
+    id,
+    superhero,
+    publisher,
+    alter_ego,
+    first_appearance,
+    characters
+  } = hero;
+
+  const imagePath = `/assets/heroes/${ id }.jpg`;
+
   return (
-    <div>
-        <h1>Hero Screen</h1>
+    <div className='row mt-5'>
+        <div className='col-4'>
+          <img 
+            src={ imagePath }
+            alt={ superhero }
+            className='img-thumbnail'
+          />
+
+        </div>
+
+        <div className='col-8'>
+          <h3>{ superhero}</h3>
+          <ul className='list-group list-group-flush'>
+            <li className='list-group-item'> <b>Alter ego:</b> { alter_ego } </li>
+            <li className='list-group-item'> <b>Publisher:</b> { publisher } </li>
+            <li className='list-group-item'> <b>First Appearance:</b> { first_appearance } </li>
+          </ul>
+
+          <h5 className='mt-3'>Characters:</h5>
+          <p>{ characters }</p>
+
+          <button 
+            className="btn btn-outline-info"
+            onClick={ handleReturn }
+          >
+            Back...
+          </button>
+        </div>
     </div>
   )
 }
