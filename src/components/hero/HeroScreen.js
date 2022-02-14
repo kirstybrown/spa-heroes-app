591c4438147d326fa3d35d6dfaa1e9b03dcea639
Kirsty Brown <kirsty.a.d.brown@gmail.com>
2022-02-14 17:49:09 +0100

    added animation to Hero List and Hero Screen

--- src/components/hero/HeroScreen.js
+++ src/components/hero/HeroScreen.js
@@ -33,12 +33,12 @@ export const HeroScreen = () => {
           <img 
             src={ imagePath }
             alt={ superhero }
-            className='img-thumbnail'
+            className='img-thumbnail animate__animated animate__bounceInLeft'
           />
 
         </div>
 
-        <div className='col-8'>
+        <div className='col-8' animate__animated animate__FadeIn>
           <h3>{ superhero}</h3>
           <ul className='list-group list-group-flush'>
             <li className='list-group-item'> <b>Alter ego:</b> { alter_ego } </li>
