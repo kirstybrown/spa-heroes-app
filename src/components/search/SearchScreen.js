afe9e918a2eac147a5240ca2273280eec20b8982
Kirsty Brown <kirsty.a.d.brown@gmail.com>
2022-02-14 18:29:57 +0100

    updated SearchScreen component

--- src/components/search/SearchScreen.js
+++ src/components/search/SearchScreen.js
@@ -1,9 +1,71 @@
 import React from 'react'
+import { HeroCard } from '../hero/HeroCard';
+import { useForm } from '../../hooks/useForm';
+import { getHeroesByName } from '../../selectors/getHeroesByName';
 
 export const SearchScreen = () => {
+
+  const [ formValues, handleInputChange ] = useForm( {
+    searchText: '',
+  } );
+
+  const { searchText } = formValues;
+
+  const heroesFiltered = getHeroesByName('STH HERE');
+
+  const handleSearch = (e) => {
+    e.preventDefault(); 
+  }
+
   return (
-    <div>
-        <h1>Search Screen</h1>
-    </div>
+    <>
+        <h1>Search</h1>
+        <hr />
+
+        <div className='row'>
+          <div className='col-5'>
+            <h4>Search</h4>
+            <hr />
+
+            <form onSubmit={ handleSearch }>
+              <input 
+                type='text'
+                placeholder='Search for a hero'
+                className='form-control'
+                name='searchText'
+                autoComplete='off'
+                value={ searchText }
+                onChange={handleInputChange}
+              />
+
+
+              <button
+                className='btn btn-outline-primary mt-1'
+                type='submit'
+                onClick={ handleSearch }
+              >
+                Search...
+              </button>
+            </form>
+
+          </div>
+
+          <div className='col-7'>
+            <h4>Results</h4>
+            <hr />
+
+            {
+              heroesFiltered.map(hero => (
+                <HeroCard 
+                  key={ hero.id }
+                  { ...hero }
+                />
+              ))
+            }
+
+
+          </div>
+        </div>
+    </>
   )
 }
