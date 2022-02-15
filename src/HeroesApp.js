02ea26d5508d2164f7fa47202700308aba82489b
Kirsty Brown <kirsty.a.d.brown@gmail.com>
2022-02-15 12:44:30 +0100

    added useReducer to HeroesApp with AuthContext.Provider to transfer reducer (user and dispatch) to child components

--- src/HeroesApp.js
+++ src/HeroesApp.js
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useReducer } from 'react'
+import { AuthContext } from './auth/authContext'
+import { authReducer } from './auth/authReducer';
 import { AppRouter } from './routers/AppRouter'
 
+
+const init = () => {
+  return {
+    logged: true,
+    name: 'Kirsty temporary'
+  }
+}
+
 export const HeroesApp = () => {
+
+  const [ user, dispatch ] = useReducer( authReducer, {}, init );
+
   return (
-    <AppRouter />
+    <AuthContext.Provider value={{
+        user,
+        dispatch
+    }}>
+          <AppRouter />
+    </AuthContext.Provider>
   )
 }
