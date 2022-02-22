a7587bf6171764b78deca7cd0394209f4624ef3f
Kirsty Brown <kirsty.a.d.brown@gmail.com>
2022-02-22 17:43:19 +0100

    completed appRouter tests

--- src/test/routers/AppRouter.test.js
+++ src/test/routers/AppRouter.test.js
@@ -5,13 +5,13 @@ import { AppRouter } from '../../routers/AppRouter';
 
 describe('Tests in <AppRouter />', () => { 
 
-    const contextValue = {
-        user: {
-            logged: false
-        }
-    }
-
     test('should show login screen if user is not authenticated', () => { 
+
+        const contextValue = {
+            user: {
+                logged: false
+            }
+        }
 
         const wrapper = mount(
             <AuthContext.Provider value={ contextValue }>
@@ -25,5 +25,24 @@ describe('Tests in <AppRouter />', () => {
      });
 
 
+     test('should show Marvel component if user is authenticated', () => { 
+
+        const contextValue = {
+            user: {
+                logged: true,
+                name: 'Pepe'
+            }
+        }
+
+        const wrapper = mount(
+            <AuthContext.Provider value={ contextValue }>
+                <AppRouter />
+            </AuthContext.Provider>
+        );
+
+        expect( wrapper ).toMatchSnapshot();
+        expect( wrapper.find('.navbar').exists() ).toBe( true );
+        
+     });
 
  })
