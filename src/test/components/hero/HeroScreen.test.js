ad3c610e389fe2798215333f5484e87371b50f98
Kirsty Brown <kirsty.a.d.brown@gmail.com>
2022-03-17 18:18:38 +0100

    added more tests to HeroScreen

--- src/test/components/hero/HeroScreen.test.js
+++ src/test/components/hero/HeroScreen.test.js
@@ -28,7 +28,7 @@ describe('tests for <HeroScreen />', () => {
         // console.log( wrapper.html() );
         expect( wrapper.find('h1').text().trim() ).toBe('No Hero Page');
 
-    })
+    });
 
     test('should show a hero if parameter exists and is found', () => { 
 
@@ -39,10 +39,42 @@ describe('tests for <HeroScreen />', () => {
                     <Route path="/" element={<h1>No Hero Page</h1>} />
                 </Routes>
             </MemoryRouter>
-        )
+        );
 
         // console.log( wrapper.html() );
         expect( wrapper.find('.row').exists() ).toBe(true);
+
+    });
+
+    test('should return to previous page', () => { 
+
+        const wrapper = mount(
+            <MemoryRouter initialEntries={['/hero/marvel-spider']}>
+                <Routes>
+                    <Route path="/hero/:heroId" element={<HeroScreen/>} />
+                    <Route path="/" element={<h1>No Hero Page</h1>} />
+                </Routes>
+            </MemoryRouter>
+        );
+
+        wrapper.find('button').prop('onClick')();
+
+        expect( mockNavigate ).toHaveBeenCalledWith(-1);
+     })
+
+     test('should show the No Hero Page if we do not have a hero', () => { 
+
+        const wrapper = mount(
+            <MemoryRouter initialEntries={['/hero/marvel-spider1234567']}>
+                <Routes>
+                    <Route path="/hero/:heroId" element={<HeroScreen/>} />
+                    <Route path="/" element={<h1>No Hero Page</h1>} />
+                </Routes>
+            </MemoryRouter>
+        )
+
+        // console.log( wrapper.html() );
+        expect( wrapper.text() ).toBe('No Hero Page');
 
     })
  })
